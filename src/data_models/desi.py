2453b9db85f29cb55ee02e730a47e84afe3ca72b
v-nils <mail@nils-voss.com>
2024-04-25 09:04:21 +0200

    Merge remote-tracking branch 'origin/ex3_1'
    # Conflicts:
    #	data/results/ex_02/kde_one_point_stats/kde_vol_with_stats_std.png
    #	src/data_models/sdss.py
    #	worksheets/ex_02.py

--- src/data_models/desi.py
+++ src/data_models/desi.py
@@ -1,10 +1,14 @@
+from typing import List, Any
+
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import pandas as pd
 import xarray as xr
 import pywt
-
+from numpy import ndarray, dtype, bool_, unsignedinteger, signedinteger, floating, complexfloating
+from pywt._doc_utils import draw_2d_wp_basis, wavedec2_keys
+import scienceplots
 from src.math_functions import average_galaxy_bands
 from src.util_functions import process_plot
 from skimage.measure import block_reduce
@@ -21,6 +25,7 @@ class Galaxy:
         :param bands: Bands of the image data.
         """
 
+        self.wavelet_data = None
         if bands is not None:
             self.bands = bands
         else:
@@ -52,30 +57,141 @@ class Galaxy:
 
         self.data = xr.concat([self.data, avg_band], dim='bands')
 
-    def compute_wavelets(self, wavelet: str = 'haar', **kwargs):
+    def compute_wavelets(self, save_path: str | None = None, **kwargs) -> list[Any]:
         """
-        Compute the wavelet transform of the galaxy data.
-        :param wavelet: The wavelet transform method.
-        :param kwargs: Additional arguments.
-        :return:
-        """
-        cA, (cH, cV, cD) = pywt.dwt2(self.data, wavelet=wavelet)
-        re_image = pywt.idwt2((cA, (cH, cV, cD)), wavelet=wavelet)
+        Compute the wavelet coefficients for the galaxy data.
 
-        wp = pywt.WaveletPacket2D(data=self.data.sel(bands=1).to_numpy(), wavelet='db1', mode='symmetric')
-        print(wp.get_level(1))
-        print(type(re_image))
-        self.wavelet_data = re_image
+        :param save_path: str: The path to save the plot.
+        :param kwargs: dict: Additional arguments.
+            level: int: The level of decomposition.
+        """
 
-    def plot_wavelet_data(self, **kwargs):
+        data = self.data[:, :, 0]
+        coef = 2
+
+        max_lev = kwargs.get('level', 2)
+
+        fig, axes = plt.subplots(2, max_lev + 1, figsize=[14, 8])
+        re_images = []
+
+        for level in range(max_lev + 1):
+            if level == 0:
+                # show the original image before decomposition
+                axes[0, 0].set_axis_off()
+                axes[0, 0].imshow(data, cmap=plt.cm.gray)
+                axes[0, 0].set_title('Image')
+                axes[0, 0].set_axis_off()
+                axes[1, 0].set_visible(False)
+                continue
+
+            coef = coef ** 2
+            c = pywt.wavedec2(data, 'db2', mode='symmetric', level=level)
+
+            # normalize each coefficient array independently for better visibility
+            c[0] /= np.abs(c[0]).max()
+            for detail_level in range(level):
+                c[detail_level + 1] = [d / np.abs(d).max() for d in c[detail_level + 1]]
+
+            cA = c[0]
+            cH, cV, cD = c[1][:]
+            re_image = pywt.waverec2(c, 'db2', mode='smooth')
+
+            re_images.append(re_image)
+
+            # show the normalized coefficients
+            arr, slices = pywt.coeffs_to_array(c)
+            axes[0, level].imshow(re_image, cmap=plt.cm.gray_r)
+            axes[0, level].set_title(f'\n 1/{coef} of wavelet coefficients')
+            axes[0, level].set_axis_off()
+
+            axes[1, level].imshow(re_image, cmap=plt.cm.gray_r)
+            axes[1, level].set_axis_off()
+
+        plt.tight_layout()
+        process_plot(plt, save_path=save_path)
+
+        return re_images
+
+    def singular_value_decomposition(self,
+                                     ranks: list | None = None,
+                                     **kwargs) -> list[Any]:
         """
-        Plot the wavelet transformed data.
+        Compute the singular value decomposition of the galaxy data.
+
+        :param ranks:
+        :param kwargs: dict: Additional arguments.
+            save_path: str: The path to save the plot.
         """
-        if hasattr(self, 'wavelet_data'):
-            plt.imshow(self.wavelet_data[:, :, 0], cmap='gray')
-            plt.show()
-        else:
-            print("Wavelet data is not computed. Please run compute_wavelets method first.")
+
+        plt.style.use(['science', 'ieee'])
+
+        svds = []
+        if ranks is None:
+            ranks = [32, 64, 128, 256]
+
+        save_path: str | None = kwargs.get('save_path', None)
+
+        data = self.data[:, :, 0]
+
+        U, S, V = np.linalg.svd(data, full_matrices=False)
+        S: np.array = np.diag(S)
+
+        hard_threshold = kwargs.get('hard_threshold', False)
+
+        if hard_threshold:
+            ht: float = 2.858 * np.median(S.diagonal())
+            print(ht)
+
+            S_ht = np.where(S > ht, S, 0)
+            approx_ht = U @ S_ht @ V
+            fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+            ax.imshow(approx_ht, cmap='gray_r')
+            ax.set_title("Hard Thresholding", fontsize=21)
+            ax.axis('off')
+
+            if save_path is not None:
+                ht_save_path = '..' + save_path.split('.')[-2] + '_ht.png'
+                process_plot(plt, save_path=ht_save_path)
+
+        if kwargs.get('plot_magnitude', False):
+            fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+            magnitudes = S.diagonal()
+            ax.plot(np.linspace(0, len(magnitudes), len(magnitudes)), magnitudes)
+            ax.set_yscale('log')
+            ax.set_xscale('log')
+            ax.set_ylabel(r'$\sigma_r$', fontsize=12)
+            ax.set_xlabel(r'RANK $r$', fontsize=12)
+
+            if hard_threshold:
+                fraction_of_svd_above_ht = np.sum(S.diagonal() > ht) / len(S.diagonal())
+                ax.axhline(y=ht, linestyle='--', label=f'Hard Threshold at {ht:.2f}')
+                ax.legend()
+                ax.set_title(f'Percentage of SVD above HT: {fraction_of_svd_above_ht*100:.2f} percent', fontsize=21)
+
+            if save_path is not None:
+                magnitude_save_path = '..' + save_path.split('.')[-2] + '_magnitude.png'
+                process_plot(plt, save_path=magnitude_save_path)
+
+        fig, ax = plt.subplots(len(ranks), 2, figsize=(8, len(ranks)*4))
+
+        for i, r in enumerate(ranks):
+            approx = U[:, :r] @ S[:r, :r] @ V[:r, :]
+            ax[i][0].imshow(approx, cmap='gray_r')
+            ax[i][0].set_title("r = " + str(r), fontsize=21)
+            ax[i, 0].axis('off')
+            ax[i][1].set_title("Original Image", fontsize=21)
+            ax[i][1].imshow(data, cmap='gray_r')
+            ax[i, 1].axis('off')
+            i += 1
+            svds.append(approx)
+
+        plt.tight_layout()
+
+        process_plot(plt, save_path=save_path)
+
+        return svds
+
+
 
     def downsample(self, p: int, bands: str | list | None = None, overwrite: bool = True, **kwargs):
         """
@@ -107,6 +223,23 @@ class Galaxy:
                                             func=np.mean)
 
             return Galaxy(downsampled_data, bands)
+
+    def plot_compare_wavelet_svd(self, wavelet_data: list[Any], svd_data: list[Any], **kwargs) -> None:
+
+        plot_cols = len(wavelet_data)
+
+        figure, axis = plt.subplots(2, plot_cols, figsize=(plot_cols * 4, 10))
+
+        for c_idx, (wavelet, svd) in enumerate(zip(wavelet_data, svd_data)):
+            axis[0, c_idx].imshow(wavelet, cmap='gray_r')
+            axis[0, c_idx].set_title('Wavelet')
+            axis[0, c_idx].axis('off')
+            axis[1, c_idx].imshow(svd, cmap='gray_r')
+            axis[1, c_idx].set_title('SVD')
+            axis[1, c_idx].axis('off')
+
+        save_path = kwargs.get('save_path', None)
+        process_plot(plt, save_path=save_path)
 
 
 class DESI:
@@ -244,6 +377,9 @@ class DESI:
 
         plt.subplots_adjust(hspace=0.05, wspace=0.05)
         process_plot(plt, save_path=save_path)
+
+
+
 
 
 
--- worksheets/ex_03.py
+++ worksheets/ex_03.py
@@ -6,7 +6,10 @@ from src.data_models.desi import DESI
 data_path = '../data/raw_data/DECals_galaxies.hdf5'
 desi_main = DESI(data_path)
 
-task = [2]
+task = [4]
+
+wavelets = None
+svds = None
 ###############################################################################
 # Exercise 1
 # Create a new DESI object with the data_path as the data_path attribute.
@@ -30,7 +33,9 @@ if 1 in task:
     desi_copy.plot_images(save_path='../data/results/ex_03/desi_rgb_avg/desi_plot.png')
 
 
-    desi_copy.create_downsampled_dataset(p_vals, plot=True, save_path='../data/results/ex_03/desi_rgb_avg/desi_downsampled.png')
+    desi_copy.create_downsampled_dataset(p_vals,
+                                         plot=True,
+                                         save_path='../data/results/ex_03/desi_rgb_avg/desi_downsampled.png')
 
 
 ###############################################################################
@@ -44,5 +49,33 @@ if 2 in task:
     galaxy = desi_copy.galaxies[galaxy_idx]
 
     galaxy.average_bands(overwrite=True)
-    galaxy.compute_wavelets()
-    galaxy.plot_wavelet_data(save_path='../data/results/ex_03/wavelets/galaxy_wavelets.png')
+    wavelets = galaxy.compute_wavelets(level=2,
+                                       save_path='../data/results/ex_03/galaxy_wavelets/desi_wavelets.png')
+
+if 3 in task:
+    galaxy_idx = 233
+
+    desi_copy = copy.deepcopy(desi_main)
+
+    galaxy = desi_copy.galaxies[galaxy_idx]
+
+    galaxy.average_bands(overwrite=True)
+
+    svds = galaxy.singular_value_decomposition(ranks=[128, 64, 32, 16, 8, 4, 2],
+                                               save_path='../data/results/ex_03/galaxy_svd/galaxy_svd_2.png',
+                                               plot_magnitude=True,
+                                               hard_threshold=True)
+
+    if wavelets is None:
+        wavelets = galaxy.compute_wavelets(level=2, save_path='../data/results/ex_03/galaxy_wavelets/galaxy_wavelets.png')
+        pass
+
+    galaxy.plot_compare_wavelet_svd(wavelets, svds, save_path='../data/results/ex_03/galaxy_wavelets_svd/desi_wavelets_svd.png')
+
+if 4 in task:
+    all_galaxies = copy.deepcopy(desi_main)
+
+    all_galaxies.average_all_galaxies(overwrite=True)
+
+    all_galaxies.sample_all_galaxies(4)
+
